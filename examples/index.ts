19223b1e52b062cdbf6acd129af1c42113b09e1e
Jose Varela <jvarela@edigma.com>
2017-06-29 17:22:03 +0100

    Refactor to include item template instead of access DOM directly; Improve example

--- examples/index.ts
+++ examples/index.ts
@@ -1,39 +1,39 @@
-import { Component } from '@angular/core';
-
 @Component({
   selector: 'app',
-  templateUrl: `
-    <div
-      ng2-carouselamos
-      [width]="500"
-      [$prev]="$prev"
-      [$next]="$next"
-    >
-      <div class="box red">
-      </div>
-      <div class="box green">
-      </div>
-      <div class="box blue">
-      </div>
-      <div class="box pink">
-      </div>
-        <div class="box red">
-      </div>
-      <div class="box green">
-      </div>
-      <div class="box blue">
-      </div>
-      <div class="box pink">
+  template: `
+    <div>
+      <div
+        ng2-carouselamos
+        class="slides-wrapper"
+        [items]="items"
+        [width]="590"
+        [$prev]="prev"
+        [$next]="next"
+        [$item]="item"
+      >
       </div>
     </div>
 
-    <ng-template #$prev>
-      <img src="assets/img/prev.png" />
+    <ng-template #prev>
+      <img src="assets/images/icon_leftarrow.png" />
     </ng-template>
-    <ng-template #$next>
-      <img src="assets/img/next.png" />
+    <ng-template #next>
+      <img src="assets/images/icon_rightarrow.png" />
     </ng-template>
-    `,
+    <ng-template #item let-item let-i="index">
+      <p>Name: {{item.name}}</p>
+      <p>Index: {{i}}</p>
+    </ng-template>
+  `
 })
-export class AppComponent {
+export class App{
+  items: Array<any> = []
+
+  constructor() {
+    this.items = [
+      { name: 'Jose' },
+      { name: 'Alice' },
+      { name: 'Bob' },
+    ]
+  }
 }
--- index.ts
+++ index.ts
@@ -2,12 +2,9 @@ import { CommonModule } from '@angular/common';
 import {
   NgModule,
   Component,
-  ViewChild,
-  AfterViewInit,
   Input,
-  ContentChild,
-  TemplateRef
 } from '@angular/core';
+import isEqual from 'lodash.isequal';
 
 @Component({
     selector: '[ng2-carouselamos]',
@@ -15,23 +12,37 @@ import {
       <style>
         .ng2-carouselamos-container {
           position: relative;
+          display: -webkit-box;
+          display: -ms-flexbox;
           display: flex;
+          -webkit-box-pack: center;
+          -ms-flex-pack: center;
           justify-content: center;
         }
         .ng2-carouselamos-wrapper {
           overflow: hidden;
         }
         .ng2-carouselamos {
+          display: -webkit-box;
+          display: -ms-flexbox;
           display: flex;
           -webkit-transition: -webkit-transform 1s;
+          transition: -webkit-transform 1s;
+          transition: transform 1s;
+          transition: transform 1s, -webkit-transform 1s;
         }
         .controls {
           position: absolute;
+          display: -webkit-box;
+          display: -ms-flexbox;
           display: flex;
           width: 100%;
+          -webkit-box-pack: justify;
+          -ms-flex-pack: justify;
           justify-content: space-between;
           top: 50%;
           left: 0;
+          -webkit-transform: translateY(-50%);
           transform: translateY(-50%);
         }
         .controls button{
@@ -41,49 +52,62 @@ import {
       </style>
       <div class="ng2-carouselamos-container">
         <div class="ng2-carouselamos-wrapper" [style.width]="width + 'px'">
-          <div class="ng2-carouselamos" #ng2Carouselamos>
-            <ng-content></ng-content>
+          <div
+            class="ng2-carouselamos"
+            [style.transform]="'translateX('+amount+'px)'"
+            [style.webkitTransform]="'translateX('+amount+'px)'"
+            #list
+          >
+            <ng-template
+              *ngFor="let item of items; let i = index"
+              [ngTemplateOutlet]="$item"
+              [ngTemplateOutletContext]="{$implicit: item, index: i}"
+            ></ng-template>
           </div>
         </div>
         <div class="controls" *ngIf="$prev || $next">
-          <button *ngIf="$prev" (click)="scroll(false)" [disabled]="amount >= 0">
+          <button *ngIf="$prev" (click)="scroll(false, list)" [disabled]="childIndex <= 0">
             <ng-template [ngTemplateOutlet]="$prev"></ng-template>
           </button>
-          <button *ngIf="$next" (click)="scroll(true)" [disabled]="amount <= -maxWidth">
+          <button *ngIf="$next" (click)="scroll(true, list)" [disabled]="childIndex >= items.length - 1">
             <ng-template [ngTemplateOutlet]="$next"></ng-template>
           </button>
         </div>
       </div>
     `
 })
-export class Ng2Carouselamos implements AfterViewInit {
-  @ViewChild('ng2Carouselamos') ng2Carouselamos;
+/*
+  *
+  * @param() items - List of items to belong in carousel
+  * @param() width - Size of window(view) to show
+  * @param() $prev - Template for previous button
+  * @param() $next - Template for next button
+  * @param() $item - Template for the item
+*/
+export class Ng2Carouselamos {
+  @Input() items = [];
   @Input() width = 500;
   @Input() $prev;
   @Input() $next;
-  @Input() maxWidth: number = 0;
+  @Input() $item;
   childIndex: number = 0;
   amount: number = 0;
 
-  ngAfterViewInit() {
-    const $ng2Carouselamos: HTMLElement = this.ng2Carouselamos.nativeElement;
-    if (!this.maxWidth)
-      this.maxWidth = Array.prototype.slice.call($ng2Carouselamos.children).map(c => c.clientWidth).reduce((prev, curr) => (prev + curr));
+  scroll(forward, elem) {
+    if (forward) {
+      this.amount -= elem.children[this.childIndex].clientWidth;
+      this.childIndex += 1;
+    } else {
+      this.childIndex -= 1;
+      this.amount += elem.children[this.childIndex].clientWidth;
+    }
   }
 
-  scroll(forward) {
-    const $child: HTMLElement = this.ng2Carouselamos.nativeElement.children[this.childIndex];
-    const nChilds = this.ng2Carouselamos.nativeElement.children.length;
-    if (forward) {
-      this.amount -= $child.clientWidth;
-      this.childIndex = (this.childIndex+1) % nChilds;
-    } else {
-      this.amount += $child.clientWidth;
-      this.childIndex = Math.abs((this.childIndex-1) % nChilds);
+  ngOnChanges(changes) {
+    if (changes.items && !isEqual(changes.items.previousValue, changes.items.currentValue)) {
+      this.amount = 0;
     }
-    this.ng2Carouselamos.nativeElement.style.webkitTransform = `translateX(${this.amount}px)`;
   }
-  
 }
 
 @NgModule({
