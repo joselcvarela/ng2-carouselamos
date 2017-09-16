bdd8be36401698cdf8117d8677c6b2146303ddcc
Jose Varela <joselcvarela@gmail.com>
2017-09-16 20:33:26 +0100

    Remove demo folder

--- demo/umd/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  template: `<tick-tock></tick-tock>`
-})
-export class AppComponent {
-  public header: string = 'UMD Demo';
-}
